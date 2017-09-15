e454d5611f937991ae668e8c34c0e73816e67eac
gmarinozzi <gmarinozzi@gmail.com>
2017-09-15 10:43:44 -0500

    puse el js del modal en un archivo propio

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-
-$(function() {
-  $("#modal-info").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
-
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
-
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
